31d79387d941300f180b61d2e14af57d3ec6290f
StynGrgv <148798338+StynGrgv@users.noreply.github.com>
2024-06-28 08:57:53 +0300

    Update esp32Firmware.ino

--- esp32Firmware.ino
+++ esp32Firmware.ino
@@ -1,6 +1,7 @@
 #include <BluetoothSerial.h>
 
 BluetoothSerial SerialBT;
+
 #define ELM_PORT SerialBT
 #define DEBUG_PORT Serial
 
@@ -9,13 +10,13 @@ BluetoothSerial SerialBT;
 #define blue 12
 #define buzzer 33
 
-
 esp_spp_sec_t sec_mask = ESP_SPP_SEC_NONE;  // or ESP_SPP_SEC_ENCRYPT|ESP_SPP_SEC_AUTHENTICATE to request pincode confirmation
 esp_spp_role_t role = ESP_SPP_ROLE_SLAVE;   // or ESP_SPP_ROLE_MASTER
 
-uint8_t Address[6] = { 0x00, 0x10, 0xcC, 0x4F, 0x36, 0x03 };
+uint8_t Address[6] = { 0x00, 0x10, 0xcC, 0x4F, 0x36, 0x03 }; // your device's MAC.
 
 int HexToDec(char* n);
+int GetCoolantTemperature(String dataFromELM);
 
 void setup() {
 
@@ -29,7 +30,7 @@ void setup() {
   // SerialBT.setPin("1234");
   ELM_PORT.begin("ESP32", true);
 
-  if (!ELM_PORT.connect(Address, 2, sec_mask, role)) {
+  if (!ELM_PORT.connect(Address, 2, sec_mask, role)) {  // if you can't connect on chanel 2, try another.
     DEBUG_PORT.println("Couldn't connect to OBD scanner - Phase 1");
     while (1)
       ;
@@ -108,7 +109,7 @@ int GetCoolantTemperature(String dataFromELM) {
 }
 
 
-int HexToDec(char* n) {
+int HexToDec(char* n) { //Convert 2 char hex number to int dec. Easiest way.
 
   Serial.println(n);
 
@@ -137,7 +138,7 @@ int HexToDec(char* n) {
       dec += 15 * 16;
       break;
     default:
-      dec += (cA - '0') * 16;
+      dec += (cA - '0') * 16; // '0' -> 48, '1' -> 49, ..... , '9' -> 57 => '9' - '0' = 9
       
       break;
   }
